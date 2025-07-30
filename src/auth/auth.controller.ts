e0a5ebb1a0e41db141313321bbfba1edb736bef3
nyeinminhtet <nyeinmg904@gmail.com>
2025-07-30 21:46:43 +0630

    add dto for auth and prisma

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthDTO } from './dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
 
-  @Post('signin')
-  signin() {
-    this.authservice.signin();
+  @Post('signup')
+  signup(@Body() dto: AuthDTO) {
+    return this.authservice.signup(dto);
   }
 
-  @Post('signup')
-  signup() {
-    this.authservice.signup();
+  @Post('signin')
+  signin() {
+    return this.authservice.signin();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   signin() {
     return { msg: 'successfully login' };
   }
 
-  signup() {
+  signup(dto: AuthDTO) {
     return { msg: 'successfully signup' };
   }
 }
