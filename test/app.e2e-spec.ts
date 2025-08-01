890ce0255994a1d946c3c6bd7faa5a084a7ef0ce
nyeinminhtet <nyeinmg904@gmail.com>
2025-08-01 23:00:49 +0630

    add Auth e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,14 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+
+import { AuthDTO } from 'src/auth/dto';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from './../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -17,11 +22,113 @@ describe('App e2e', () => {
     );
 
     await app.init();
+    await app.listen(3333);
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    const dto: AuthDTO = {
+      email: 'test@test.com',
+      password: '123456',
+    };
+    describe('SignUp', () => {
+      it('should throw 400 error if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            passport: dto.password,
+          })
+          .expectStatus(400);
+      });
+      it('should throw 400 error if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('should throw 400 error if body is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({})
+          .expectStatus(400);
+      });
+      it('should sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('SignIn', () => {
+      it('should throw 400 error if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+      it('should throw 400 error if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('should throw 400 error if body is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({})
+          .expectStatus(400);
+      });
+      it('should sign in', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('token', 'access_token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get Me', () => {
+      it.todo('should pass');
+    });
+
+    describe('Edit User', () => {
+      it.todo('should pass');
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create Bookmarks', () => {});
+
+    describe('Get Bookmarks', () => {});
+
+    describe('Get Bookmark by id', () => {});
+
+    describe('Edit Bookmarks', () => {});
+
+    describe('Delete Bookmarks', () => {});
+  });
 });
